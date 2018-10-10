0ba6c122d32e1c6b39e31e0d0e2f4744d8f2b5a8
Shinye Song <shinye.song@croquis.com>
2018-10-10 22:53:45 +0900

    festa 링크 추가

--- src/app/components/Camp.js
+++ src/app/components/Camp.js
@@ -4,7 +4,7 @@ export default class DevFest extends Component {
   render() {
     return (
       <div class="camp">
-        <a href="#">
+        <a href="https://festa.io/events/88" target="_blank">
           <span>티켓 구입 바로가기</span>
         </a>
       </div>
