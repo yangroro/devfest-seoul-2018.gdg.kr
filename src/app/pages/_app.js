73dd90b1f9976d78c524286f468ec9bf5c0603f1
kevin <tost8295@gmail.com>
2018-10-12 01:15:54 +0900

    Add Navbar functionality

--- src/app/pages/_app.js
+++ src/app/pages/_app.js
@@ -3,6 +3,7 @@ import App, { Container } from 'next/app'
 import SideNav from '../components/SideNav'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { NavProvider } from '../NavProvider'
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -19,12 +20,14 @@ class MyApp extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <Container>
-        <Component {...pageProps} />
-        <Header />
-        <SideNav />
-        <Footer />
-      </Container>
+      <NavProvider>
+        <Container>
+          <Component {...pageProps} />
+          <Header />
+          <SideNav />
+          <Footer />
+        </Container>
+      </NavProvider>
     )
   }
 }
