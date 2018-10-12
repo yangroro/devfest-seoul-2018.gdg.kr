2bccbea1873c0b6b3a2f4dbdaee7c96c84d7a3ce
kevin <tost8295@gmail.com>
2018-10-13 03:57:38 +0900

    get rid of useless info

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class Footer extends Component {
   render() {
@@ -8,16 +8,18 @@ export default class Footer extends Component {
           <div className="footer-info">
             <div className="contact">
               <h1>Contact us</h1>
-              <p><a href="mailto:devfest-seoul-2018@googlegroups.com">devfest-seoul-2018@googlegroups.com</a></p>
+              <p>
+                <a href="mailto:devfest-seoul-2018@googlegroups.com">
+                  devfest-seoul-2018@googlegroups.com
+                </a>
+              </p>
             </div>
             <div className="logo">
               <img src="/static/gdg-logo.png" width="215px" />
             </div>
           </div>
           <p className="copyright">
-          <b>© DevFest Seoul 2018</b> <br />
-          Designed: Eunjeong Kim, Nayeon Kim <br />
-          Developed: Eun Cho, Heechan Yang, Kevin Jin, Shinye Song <br />
+            <b>© DevFest Seoul 2018</b> <br />
           </p>
         </div>
       </footer>
